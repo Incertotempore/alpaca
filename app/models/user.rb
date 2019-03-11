aebf86c7ee3dffb21a39249c189550c12914179d
ivano panizza <ivano.panizza@libero.it>
2019-03-11 16:34:29 +0100

    review model updated with user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :deals
+  has_many :items, through: :deals
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_150427) do
+ActiveRecord::Schema.define(version: 2019_03_11_152831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,9 @@ ActiveRecord::Schema.define(version: 2019_03_11_150427) do
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +68,5 @@ ActiveRecord::Schema.define(version: 2019_03_11_150427) do
   add_foreign_key "deals", "users"
   add_foreign_key "items", "users"
   add_foreign_key "reviews", "items"
+  add_foreign_key "reviews", "users"
 end

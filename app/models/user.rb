3604350740d50b5dcac8dd6a5a8966ccbe265089
ivano panizza <ivano.panizza@libero.it>
2019-03-12 15:52:11 +0100

    home design done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :deals
-  has_many :items, through: :deals
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

f567c8f89c95f7e72e74e500ba5b348df6161904
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-03-12 12:41:48 +0100

    items#index

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,23 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def my_items
+  end
+
+  def my_item
+  end
+
+  def patch
+  end
+end

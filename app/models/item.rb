57d0ce164f38935a941d9b8e5435633b1b73fac6
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-03-14 16:21:20 +0100

    date picker in deals

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,5 @@ class Item < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   mount_uploader :photo, PhotoUploader
+  belongs_to :deal
 end

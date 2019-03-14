a5dcb0acf340996803bee90dd67e9870d11871fc
giacomo montemurri <gia94.montemurri@yahoo.it>
2019-03-14 14:16:38 +0100

    reviews working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :sanitize_params, if: :devise_controller?
+
+
+
+def sanitize_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:address])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:address])
+
 end
+
+  end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :item
+
+
+  def creator
+    User.find(self.user_id).first_name
+  rescue
+    "no user assigned"
+  end
 end

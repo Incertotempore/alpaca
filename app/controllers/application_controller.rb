898a4a36f887e190cde1b9e7bb61ebbecfc75f71
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-03-15 14:42:02 +0100

    user photo working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :sanitize_params, if: :devise_controller?
 
   def sanitize_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:address])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:address])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,:address, :photo, :photo_cache])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name,:address, :photo, :photo_cache])
   end
 end

19559a0beb0b886d1193bae3faef08aff0935b77
nicolas lequeux <contact@nicolaslequeux.com>
2019-03-15 16:31:21 +0100

    End of day

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -18,8 +18,9 @@ const initMapbox = () => {
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
+
     markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
